ac745ba9600ef564b3037589d0cf17e88b93ea0f
Debadatta17 <debadattadash17@gmail.com>
2026-03-25 13:20:56 +0530

    add sub heading

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1 className='text-blue-600 dark:text-sky-400 flex justify-center items-center font-extrabold  text-8xl'>I am Dev.</h1>
+      <h2>I am a Full stack Developer.</h2>
     </>
   )
 }
